f78cecff640a6327223ac3fd64a8d4653fe719f8
gnvvs-07 <vinaysankargokavarapu@gmail.com>
2024-10-17 22:52:31 +0530

    teacher mode and player mode buttons UI completed

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,12 +1,33 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
+import { usePathname } from "next/navigation"
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 // user button
 
 const NavbarRoutes = () =>{
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isTeacherPage = pathname?.startsWith("/teacher");
+    const isPlayerPage = pathname?.includes("/chapter");
     return(
         <div className="flex gap-x-2 ml-auto">
+            {isTeacherPage || isPlayerPage ?(
+                <Button variant={"destructive"}>
+                    <LogOut/>Exit
+                </Button>
+            ):(
+                <Link href="/teacher/courses">
+                    <Button variant={"gradient"} className="text-bold text-white text-xs">
+                        Teacher mode
+                    </Button>
+                </Link>
+            )}
             <UserButton/>
         </div>
     )
