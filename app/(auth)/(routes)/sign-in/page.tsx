6e97557ccceded56b13e7e11ad01d2eab4c0c16d
gnvvs-07 <vinaysankargokavarapu@gmail.com>
2024-10-17 17:53:14 +0530

    Authentication completed using clerk

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SignIn() {
-    return (
-      <div>
-        <p className = "text-red-500">This is a unprotected page</p>
-      </div>
-    )
-  }
-  
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
       <p className = "text-red-500">This is a protected page</p>
+      <UserButton/>
     </div>
   )
 }
