c269c01e715b5c9bf47b6500b81c56543efb0153
gnvvs-07 <vinaysankargokavarapu@gmail.com>
2024-10-17 16:22:43 +0530

    Created the project and started Folder structure going to authentication

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+export default function SignIn() {
+    return (
+      <div>
+        <p className = "text-red-500">This is a unprotected page</p>
+      </div>
+    )
+  }
+  
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p className = "text-red-500">This is a protected page</p>
+    </div>
+  )
+}
